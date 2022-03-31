1759e16c02110f04e845699da1a79eddd84f217c
licwim <licwimm@gmail.com>
2022-03-31 04:11:23 +0300

    aiomysql instead of pymysql

--- fsb/db/__init__.py
+++ fsb/db/__init__.py
@@ -1,10 +1,16 @@
 # !/usr/bin/env python
 
-from peewee import MySQLDatabase
+from peewee_async import MySQLDatabase
 from playhouse.migrate import MySQLMigrator
 
 from fsb.config import Config
 
-base_db = MySQLDatabase(Config.db_name, user=Config.db_user, password=Config.db_password,
-                        host=Config.db_host, port=3306)
+base_db = MySQLDatabase(
+    database=Config.db_name,
+    user=Config.db_user,
+    password=Config.db_password,
+    host=Config.db_host,
+    port=3306
+)
+
 base_migrator = MySQLMigrator(base_db)
