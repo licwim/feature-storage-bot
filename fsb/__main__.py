96d0defce56f5c56a3e2cf73040ca478c9107182
licwim <licwimm@gmail.com>
2023-04-26 11:19:24 +0300

    init fix

--- fsb/__main__.py
+++ fsb/__main__.py
@@ -1,13 +1,9 @@
 # !/usr/bin/env python
 
 from fsb.app import FeatureStorageBot
-from fsb.config import init_config, Config
-from fsb.logger import init_logger
 
 
 def main():
-    init_logger(False, Config)
-    init_config()
     app = FeatureStorageBot()
     app.run()
 
